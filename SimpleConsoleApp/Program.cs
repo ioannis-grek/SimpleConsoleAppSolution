0446de073ae1df4456c1a8f29c99ffcc638376cb
ioannis-grek <ioannis_1975@hotmail.com>
2020-04-11 21:09:03 +0300

    Added color asking session.

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -11,9 +11,20 @@ namespace SimpleConsoleApp
         static void Main(string[] args)
         {
             string name;
+            string color;
             Console.Write("Hello, what is your name? ");
             name = Console.ReadLine();
             Console.WriteLine("Hello, {0} nice to meet you.", name);
+            Console.Write("What is your favorite color? ");
+            color = Console.ReadLine();
+            if(color == "Red" || color == "Red")
+            {
+                Console.WriteLine("That's great, {0} is my favorite color too", color);
+            }
+            else
+            {
+                Console.WriteLine("Very nice, {0} is a nice color.", color);
+            }
             Console.ReadKey();
         }
     }
