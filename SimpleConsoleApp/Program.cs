e65628d1906198954e8c2a0b8606d67fb93faecf
ioannis-grek <ioannis_1975@hotmail.com>
2020-04-11 20:37:01 +0300

    Add project files.

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Hello, what is your name? ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello, {0} nice to meet you.", name);
+            Console.ReadKey();
+        }
+    }
+}
